27c7c03d80aa8544fc79cb567575f1c11e0b9b11
Sshahparnia <shervan.shahparnia@gmail.com>
2024-11-14 17:31:40 -0800

    updated dashboard, homepage, login, added navbar with themecontext in preparation for application wide darkmode feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
+// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/NavBar";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
 export default App;
-
-
