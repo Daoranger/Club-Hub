57729ad2e8a2bf438d05075bdeed50ef2a7aa286
DaOranger <132038723+Daoranger@users.noreply.github.com>
2024-11-15 17:43:27 -0800

    Setup .env file for local DB Connection

--- backend/database.js
+++ backend/database.js
@@ -1,18 +1,26 @@
 import express from "express";
 import mysql from "mysql2";
 import dotenv from "dotenv";
-
-// App configurationc
+// App configuration
 const app = express();
 dotenv.config();
 
 // DB connection; make sure to set up .env variables
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
+const dbCon = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Testing DB connection
+dbCon.connect(function(err) {
+  if(err) {
+    console.log("DB Connection error!");
+  } else {
+    console.log("DB Connection are good!");
+  }
+});
 
 // Backend connection
 app.get("/", (req,res)=> {
